dde4d68de7f98370f43a20a3c6f0463fd8dd8156
Yehor Lvivski <lvivski@gmail.com>
2013-10-21 02:26:11 +0300

    fix amd

--- davy.js
+++ davy.js
@@ -1,13 +1,17 @@
 (function(global) {
   "use strict";
+  var nextTick;
   if (typeof define === "function" && define.amd) {
-    define(Promise);
+    define([ "subsequent" ], function(subsequent) {
+      nextTick = subsequent;
+      return Promise;
+    });
   } else if (typeof module === "object" && module.exports) {
     module.exports = Promise;
-    var nextTick = require("subsequent");
+    nextTick = require("subsequent");
   } else {
     global.Davy = global.Promise = Promise;
-    var nextTick = global.nextTick;
+    nextTick = global.nextTick;
   }
   function Promise(value) {
     this.value = value;
